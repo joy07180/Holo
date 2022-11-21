42069106672dfee8754a482a0a492db6352077f3
joy07180 <110527932+joy07180@users.noreply.github.com>
2022-11-21 12:03:08 +0900

    Update T_BoardServiceImpl.java

--- Holo/src/main/java/service/T_BoardServiceImpl.java
+++ Holo/src/main/java/service/T_BoardServiceImpl.java
@@ -7,90 +7,25 @@ import org.springframework.stereotype.Service;
 
 import criTest.Criteria;
 import criTest.SearchCriteria;
-import mapperInterface.T_BoardMapper;
-import vo.T_BoardVO;
+import mapperInterface.Tip_BoardMapper;
+import vo.Tip_BoardVO;
 
 @Service
-public class T_BoardServiceImpl implements T_BoardService {
-	// ** T_Blist
-	
+public class Tip_BoardServiceImpl implements Tip_BoardService {
 	@Autowired
-	T_BoardMapper mapper;
-	
-	// ** Board Check List
- 	//@Override
-	//public List<T_BoardVO> checkList(T_BoardVO vo){
-	//	return mapper.checkList(vo);
-	//}
-	
-	
-	@Override 
-	public List<T_BoardVO> checkList(SearchCriteria cri){
- 		return mapper.checkList(cri);
- 	}
-//	@Override 
-//	public List<T_BoardVO> checkList2(SearchCriteria cri){
-// 		return mapper.checkList(cri);
-// 	}
-	@Override
-	public int checkCount(SearchCriteria cri) {
-		return mapper.checkCount(cri);
-	}
+	Tip_BoardMapper mapper;
 	
 	// ** Criteria PageList
-	@Override 
-	public List<T_BoardVO> searchList(SearchCriteria cri) {
+	@Override // ver02
+	public List<Tip_BoardVO> searchList(SearchCriteria cri) {
 		return mapper.searchList(cri);
 	}
 	public int searchCount(SearchCriteria cri) {
 		return mapper.searchCount(cri);
 	}
-//	@Override 
-//	public List<T_BoardVO> searchList2(SearchCriteria cri) {
-//		return mapper.searchList(cri);
-//	}
-	
-	// ** selectOne
-	@Override
-	public T_BoardVO selectOne(T_BoardVO vo) {
-		return mapper.selectOne(vo);
-	}
-	
-	// ** Insert
-	@Override
-	public int insert (T_BoardVO vo) {
-		return mapper.insert(vo);
-	}
-	// ** Update
-	@Override
-	public int update(T_BoardVO vo) {
-		return mapper.update(vo);
-	}
-	
-	// ** Delete
-	@Override
-	public int delete(T_BoardVO vo) {
-		return mapper.delete(vo);
-	}
-	
-	// ** 조회수 증가
-	@Override
-	public int countUp(T_BoardVO vo) {
-		return mapper.countUp(vo);
-	}
-	
-	// ** 답글 등록
-	@Override
-	public int rinsert(T_BoardVO vo) {
-		int result = mapper.rinsert(vo);
-		if ( result>0 )
-			System.out.println("** stepUpdate Count => "+ mapper.stepUpdate(vo));
-		else result=0;
-		return result;
-	}
 	
 	@Override // ver01
-	public List<T_BoardVO> criList(Criteria cri) {
+	public List<Tip_BoardVO> criList(Criteria cri) {
 		return mapper.criList(cri);
 	}
 	public int criTotalCount() {
@@ -99,11 +34,56 @@ public class T_BoardServiceImpl implements T_BoardService {
 	
 	// ** selectList
 	@Override
-	public List<T_BoardVO> selectList() {
+	public List<Tip_BoardVO> selectList() {
 		return mapper.selectList();
 	}
+	// ** selectOne
+	@Override
+	public Tip_BoardVO selectOne(Tip_BoardVO vo) {
+		return mapper.selectOne(vo);
+	}
 	
-
+	// ** Insert
+	@Override
+	public int insert(Tip_BoardVO vo) {
+		return mapper.insert(vo);
+	}
+	// ** Update
+	@Override
+	public int update(Tip_BoardVO vo) {
+		return mapper.update(vo);
+	}
+	// ** Delete
+	@Override
+	public int delete(Tip_BoardVO vo) {
+		return mapper.delete(vo);
+	}
+	// ** 조회수 증가
+	@Override
+	public int countUp(Tip_BoardVO vo) {
+		return mapper.countUp(vo);
+	}
 	
-
-} //class
+	// ** 답글 등록
+	@Override
+	public int rinsert(Tip_BoardVO vo) {
+		int result = mapper.rinsert(vo);
+		if ( result>0 )
+			System.out.println("** stepUpdate Count => "+ mapper.stepUpdate(vo));
+		else result=0;
+		return result;
+	}
+	
+	// home 출력 최신글
+	@Override
+	public List<Tip_BoardVO> selectHList(){
+		return mapper.selectHList();
+	}	
+	
+	// home 출력 인기글
+	@Override
+	public List<Tip_BoardVO> selectHhotList(){
+		return mapper.selectHhotList();
+	}
+	
+}
